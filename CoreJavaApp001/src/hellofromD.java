c99054ad2ee09325d5b50e20826627c4f8e2af76
dhnnjr <dhnnjr870@gmail.com>
2021-10-28 13:08:00 +0530

    this is my 2nd commit for a pull rerquest

--- CoreJavaApp001/src/hellofromD.java
+++ CoreJavaApp001/src/hellofromD.java
@@ -3,7 +3,8 @@ public class hellofromD {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello from Dhananjay");
+		System.out.println("Hello from Dhananjay Rai");
+		System.out.println("trying to give a pull request");
 	}
 
 }
