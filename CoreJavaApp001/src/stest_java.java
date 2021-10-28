26fefe837967f8cb0449131a5646e745a2a2fdd3
saibhagawan97 <saibhagawan2222@gmail.com>
2021-10-28 18:38:07 +0530

    testing merge by creating a new_branch
    Signed-off-by: saibhagawan97 <saibhagawan2222@gmail.com>

--- CoreJavaApp001/src/stest_java.java
+++ CoreJavaApp001/src/stest_java.java
@@ -4,7 +4,7 @@ public class stest_java {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("testing project");
-
+		System.out.println("test got success, hurrayy!!");
 	}
 
 }
