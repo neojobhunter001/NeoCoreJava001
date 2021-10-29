5514ad01aa695b06e3529f9c1cf47a3021bec6de
dhnnjr <dhnnjr870@gmail.com>
2021-10-29 19:13:30 +0530

    5th commit

--- CoreJavaApp001/src/djr_java.java
+++ CoreJavaApp001/src/djr_java.java
@@ -3,7 +3,7 @@ public class djr_java {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("i love coding!!!");
+		System.out.println("i love coding!!");
 	}
 
 }
